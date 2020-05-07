3f387a643570b87a3dde852b72bf2a0216b2b118
KaynirGames <kaynir.games@gmail.com>
2020-05-07 22:15:13 +0300

    Поправил коллайдеры тайлов, реализовал лазерную ловушку

---
+++ Assets/Scripts/LaserTrap.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrap : MonoBehaviour
+{
+    public float laserDelay;
+    public Collider2D laserBeam;
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField, Tooltip("Задержка между срабатыванием ловушки")] float trapActDelay;
+    
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.GetComponent<Player>().Die();
+        }
+    }
+    /// <summary>
+    /// Событие в анимации, меняет текущий статус ловушки на противоположный
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator ChangeTrapStatus()
+    {
+        yield return new WaitForSeconds(trapActDelay);
+        animator.SetBool("isTrapOn", !animator.GetBool("isTrapOn"));
+    }
+}
