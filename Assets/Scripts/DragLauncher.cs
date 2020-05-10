a06625b01bb9c32bf0478e53d5b8adf6ab5d23ca
KaynirGames <kaynir.games@gmail.com>
2020-05-10 19:06:35 +0300

    Подправил анимацию и скрипт перемещения игрока

---
+++ Assets/Scripts/DragLauncher.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DragLauncher : MonoBehaviour
+{
+    /// <summary>
+    /// Максимальный радиус натяжения
+    /// </summary>
+    [SerializeField] private float maxDragRadius = 2.5f;
+    /// <summary>
+    /// Максимальная сила запуска
+    /// </summary>
+    [SerializeField] private float maxLaunchForce = 15f;
+    /// <summary>
+    /// Начальная точка натяжения
+    /// </summary>
+    private Vector2 startDragPoint;
+    /// <summary>
+    /// Конечная точка натяжения
+    /// </summary>
+    private Vector2 endDragPoint;
+    /// <summary>
+    /// Траектория запуска
+    /// </summary>
+    private LineRenderer launchTrajectory;
+
+    private void Start()
+    {
+        launchTrajectory = GetComponent<LineRenderer>();
+    }
+
+    /// <summary>
+    /// Устанавливает начальную точку натяжения запускаемого объекта
+    /// </summary>
+    public void SetStartDragPoint(Vector3 launchObjectPos)
+    {
+        startDragPoint = launchObjectPos;
+
+        launchTrajectory.enabled = true;
+        launchTrajectory.SetPosition(0, startDragPoint);
+    }
+    /// <summary>
+    /// Устанавливает конечную точку натяжения запускаемого объекта
+    /// </summary>
+    public void SetEndDragPoint()
+    {       
+        endDragPoint = CorrectEndPoint();
+        launchTrajectory.SetPosition(1, endDragPoint);
+    }
+    /// <summary>
+    /// Рассчитывает силу запуска объекта
+    /// </summary>
+    public Vector2 GetLaunchForce()
+    {
+        launchTrajectory.enabled = false;
+
+        Vector2 direction = (startDragPoint - endDragPoint).normalized;
+        float distance = Vector2.Distance(startDragPoint, endDragPoint);
+        float launchForceFactor = Mathf.Clamp01(distance / maxDragRadius);
+
+        return direction * launchForceFactor * maxLaunchForce;
+    }
+    /// <summary>
+    /// Корректирует конечную точку при превышении максимального радиуса натяжения
+    /// </summary>
+    private Vector2 CorrectEndPoint()
+    {
+        Vector2 currentPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Vector2.Distance(startDragPoint, currentPoint) > maxDragRadius)
+        {
+            return startDragPoint + (currentPoint - startDragPoint).normalized * maxDragRadius;
+        }
+        else
+        {
+            return currentPoint;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,37 +10,53 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     private Collider2D col;
+    private Animator animator;
 
-    [HideInInspector] public Animator animator;
+    private DragLauncher dragLauncher;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
+        dragLauncher = GetComponent<DragLauncher>();
+    }
+
+    private void Update()
+    {
+        if (IsGrounded())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragLauncher.SetStartDragPoint(transform.position);
+                animator.SetBool("isLaunching", true);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                dragLauncher.SetEndDragPoint();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                Launch(dragLauncher.GetLaunchForce());
+                animator.SetBool("isLaunching", false);
+                animator.SetBool("wasLaunched", true);
+            }
+        }
     }
 
     /// <summary>
     /// Запускает игрока с заданной силой
     /// </summary>
-    public void Launch(Vector2 force)
+    private void Launch(Vector2 force)
     {
         rb.AddForce(force, ForceMode2D.Impulse);
-    }
-
-    public void Die()
-    {
-        //death particle effect
-        Destroy(gameObject);
-        //убрать рестарт в класс-менеджер сцен
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     /// <summary>
     /// Проверяет, находится ли персонаж на земле
     /// </summary>
     /// <returns></returns>
-    public bool IsGrounded()
+    private bool IsGrounded()
     {
         Collider2D collider = Physics2D.OverlapBox(groundCheckPoint.transform.position, col.bounds.extents, 0, groundLayerMask);
         if (collider != null)
@@ -60,5 +76,13 @@ public class Player : MonoBehaviour
     public void OnLanding()
     {
         animator.SetBool("wasLaunched", false);
+    }
+
+    public void Die()
+    {
+        //death particle effect
+        Destroy(gameObject);
+        //убрать рестарт в класс-менеджер сцен
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
